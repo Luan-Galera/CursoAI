a89f44f07fa5deea22bf60cf5a6cd40d521a3761
Luan-Galera <luangalera17@gmail.com>
2026-06-01 17:05:24 -0300

    Adiciona script de exemplo de notebook Jupyter para integração com a API Groq
    - Criado um novo script Python exemplo_jupyter.py no diretório Aula03.
    - Implementado um exemplo básico de uso da API Groq para gerar uma resposta sobre linguagens de IA.
    - Utilizado o pandas para formatar a resposta em um DataFrame para possível análise adicional.

---
+++ Aula03/exemplo_jupyter.py
@@ -0,0 +1,24 @@
+from groq import Groq
+import pandas as pd
+
+client = Groq(
+    api_key = "<SUA_API_AQUI>"
+)
+
+response = client.chat.completions.create(
+    model = "openai/gpt-oss-120b",
+    messages = [
+        {
+            "role": "user",
+            "content": "Liste as 5 linguagens utilizadas em IA."
+        }
+    ]
+)
+
+texto = response.choices[0].message.content
+
+df = pd.DataFrame({
+    "Resposta": [texto]
+})
+
+print(texto)
